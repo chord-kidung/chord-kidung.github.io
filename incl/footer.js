82fdb685a0e4dfa4493f8a30a1fe87d9141660ba
chord-kidung <94449218+chord-kidung@users.noreply.github.com>
2021-11-16 22:32:59 +0700

    Add files via upload

--- incl/footer.js
+++ incl/footer.js
@@ -1,5 +1,5 @@
 function footer(){
-    var footmenu=['About|||/doc/about/about.html','Privacy policy|||/doc/privacy-policy/ppol.html','Contact|||/doc/contact/contact.html'],footr=document.createElement('footer'),botmmn=document.createElement('div');
+    var footmenu=['About|||/doc/about/','Privacy policy|||/doc/privacy-policy/','Contact|||/doc/contact/'],footr=document.createElement('footer'),botmmn=document.createElement('div');
     document.body.insertAdjacentElement('beforeend',footr);
     botmmn.classList.add('footer-menu');
     footr.appendChild(botmmn);
