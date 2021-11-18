7ae720667ea7ee5051cf2499dc76fbf2aaf34be7
chord-kidung <94449218+chord-kidung@users.noreply.github.com>
2021-11-18 12:38:28 +0700

    Add files via upload

--- incl/sidebar.js
+++ incl/sidebar.js
@@ -1,10 +1,3 @@
 function sidebar(){
-    const testFolder = '/';
-    const fs = require('fs');
-
-    fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-        console.log(file);
-    });
-    });
+    
 }
