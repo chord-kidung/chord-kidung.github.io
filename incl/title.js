bf7934b48671b448ff92e448376d17050bf9147a
chord-kidung <94449218+chord-kidung@users.noreply.github.com>
2021-11-20 12:21:01 +0700

    Add files via upload

--- incl/title.js
+++ incl/title.js
@@ -2,10 +2,11 @@ function titlegen(){
     var pagetitle=document.createElement('title');
     if(ptype=='post')pagetitle.innerText=`${post_data.posts[pn].title} | ${post_data.blog[0].title}`;
     else if(ptype=='home')pagetitle.innerText=`Halaman Utama | ${post_data.blog[0].title}`;
-    else if(ptype='doc'){
+    else if(ptype=='doc'){
         if(pdiv=='about')pagetitle.innerText=`Tentang ${post_data.blog[0].title}`;
-        else if(pdiv='tos')pagetitle.innerText=`Syarat dan Ketentuan | ${post_data.blog[0].title}`;
+        else if(pdiv=='tos')pagetitle.innerText=`Syarat dan Ketentuan | ${post_data.blog[0].title}`;
     }
+    else if(ptype=='404')pagetitle.innerText=`404 | ${post_data.blog[0].title}`;
     else pagetitle.innerText='TEST';
     document.head.appendChild(pagetitle);
 }
